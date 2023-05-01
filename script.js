df4cee280bfc897c2bc3b2772632b17541f6f29b
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-01 17:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// get the size info element
+const sizeInfo = document.getElementById('sizeInfo');
+
+// function to update the size of the info text
+
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+
+// update the size info text on page load
+updateSize();
+
+// add an event listener for the window resize event
+window.addEventListener("resize", updateSize);
